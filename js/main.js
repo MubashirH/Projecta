20531f360c91606391ee57c28cdcf526801c279c
Mubashir <mubashir@riafy.me>
2022-03-10 18:50:00 +0300

    swiper slider added to the home page

--- js/main.js
+++ js/main.js
@@ -6,4 +6,21 @@ function headerFooterLoad() {
 $(document).ready(function () {
   AOS.init();
   headerFooterLoad();
+  const bannerSwiper = new Swiper(".bannerSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    effect: "fade",
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      grabCursor: true,
+      dynamicBullets: true,
+    },
+  });
 });
